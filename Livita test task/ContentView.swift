5eedede29012dcc7741e46dd75844f6e3b46b027
makson <94912293+Maksymkek@users.noreply.github.com>
2026-06-16 17:09:41 +0300

    Initial Commit

---
+++ Livita test task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Livita test task
+//
+//  Created by Максим Грищенков on 16.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Livita test task/Livita_test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Livita_test_taskApp.swift
+//  Livita test task
+//
+//  Created by Максим Грищенков on 16.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Livita_test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
